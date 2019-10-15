773e2ee042fa6cc80f7a5bec93717bb6bd8ade76
Alex Sandberg <alexsandberg7@gmail.com>
2019-10-15 13:57:19 -0600

    Add show creation functionality

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -579,10 +579,28 @@ def create_show_submission():
     # called to create new shows in the db, upon submitting new show listing form
     # TODO: insert form data as a new Show record in the db, instead
 
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
+    try:
+        artist_id = request.form['artist_id']
+        venue_id = request.form['venue_id']
+        start_time = request.form['start_time']
+
+        show = Show(artist_id=artist_id, venue_id=venue_id,
+                    start_time=start_time)
+
+        db.session.add(show)
+        db.session.commit()
+
+        # on successful db insert, flash success
+        flash('Show was successfully listed!')
+    except:
+        db.session.rollback()
+
+        flash('An error occurred. Show could not be listed.')
+    finally:
+        db.session.close()
+
     # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
+    # e.g.,
     # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
     return render_template('pages/home.html')
 
