a21494a069f6fa9ab0383afb9e0237535bf79aad
Alex Sandberg <alexsandberg7@gmail.com>
2019-10-16 09:56:26 -0600

    Replace temp data with real on artists overview page

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -330,7 +330,7 @@ def create_venue_submission():
         state = request.form['state']
         address = request.form['address']
         phone = request.form['phone']
-        genres = list(form.genres.data)
+        genres = form.genres.data
         print('NEW GENRES', genres)
         facebook_link = request.form['facebook_link']
 
@@ -371,16 +371,28 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
     # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
+
+    # data = [{
+    #     "id": 4,
+    #     "name": "Guns N Petals",
+    # }, {
+    #     "id": 5,
+    #     "name": "Matt Quevedo",
+    # }, {
+    #     "id": 6,
+    #     "name": "The Wild Sax Band",
+    # }]
+
+    data = []
+
+    artists = Artist.query.all()
+
+    for artist in artists:
+        data.append({
+            "id": artist.id,
+            "name": artist.name
+        })
+
     return render_template('pages/artists.html', artists=data)
 
 
